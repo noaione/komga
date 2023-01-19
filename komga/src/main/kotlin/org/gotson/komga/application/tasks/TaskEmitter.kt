0696a5cc494e6c332f16136d5c6e2a224c7642e3
noaione <noaione0809@gmail.com>
2023-01-19 14:36:36 +0800

    feat: allow disabling global find duplicate pages
    Maybe move it to per-library settings instead.

--- komga/src/main/kotlin/org/gotson/komga/application/tasks/TaskEmitter.kt
+++ komga/src/main/kotlin/org/gotson/komga/application/tasks/TaskEmitter.kt
@@ -12,6 +12,7 @@ import org.gotson.komga.domain.model.SearchContext
 import org.gotson.komga.domain.model.SearchOperator
 import org.gotson.komga.domain.persistence.BookRepository
 import org.gotson.komga.domain.service.BookConverter
+import org.gotson.komga.infrastructure.configuration.KomgaProperties
 import org.gotson.komga.infrastructure.jooq.UnpagedSorted
 import org.gotson.komga.infrastructure.search.LuceneEntity
 import org.springframework.context.ApplicationEventPublisher
@@ -26,6 +27,7 @@ class TaskEmitter(
   private val bookConverter: BookConverter,
   private val tasksRepository: TasksRepository,
   private val eventPublisher: ApplicationEventPublisher,
+  private val komgaProperties: KomgaProperties,
 ) {
   fun scanLibrary(
     libraryId: String,
@@ -122,7 +124,8 @@ class TaskEmitter(
     library: Library,
     priority: Int = DEFAULT_PRIORITY,
   ) {
-    submitTask(Task.FindDuplicatePagesToDelete(library.id, priority))
+    if (komgaProperties.findDuplicatePages)
+      submitTask(Task.FindDuplicatePagesToDelete(library.id, priority))
   }
 
   fun removeDuplicatePages(
