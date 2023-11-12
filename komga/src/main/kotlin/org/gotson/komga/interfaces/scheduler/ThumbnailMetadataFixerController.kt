8a3b35a501803ce76a671c2204761570b652f80b
noaione <noaione0809@gmail.com>
2023-11-12 10:58:00 +0700

    feat: allow thumbnail meta fix skip

--- komga/src/main/kotlin/org/gotson/komga/interfaces/scheduler/ThumbnailMetadataFixerController.kt
+++ komga/src/main/kotlin/org/gotson/komga/interfaces/scheduler/ThumbnailMetadataFixerController.kt
@@ -3,6 +3,7 @@ package org.gotson.komga.interfaces.scheduler
 import mu.KotlinLogging
 import org.gotson.komga.application.tasks.LOWEST_PRIORITY
 import org.gotson.komga.application.tasks.TaskEmitter
+import org.gotson.komga.infrastructure.configuration.KomgaProperties
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
@@ -14,11 +15,16 @@ private val logger = KotlinLogging.logger {}
 @Component
 class ThumbnailMetadataFixerController(
   private val taskEmitter: TaskEmitter,
+  private val komgaProperties: KomgaProperties,
 ) {
 
   @EventListener(ApplicationReadyEvent::class)
   fun findThumbnailsWithoutMetadata() {
-    logger.info { "Find and fix thumbnails without metadata" }
-    taskEmitter.fixThumbnailsWithoutMetadata(LOWEST_PRIORITY)
+    if (komgaProperties.thumbnailMetadataStartupFix) {
+      logger.info { "Find and fix thumbnails without metadata" }
+      taskEmitter.fixThumbnailsWithoutMetadata(LOWEST_PRIORITY)
+    } else {
+      logger.info { "Skipping startup thumbnail metadata fix" }
+    }
   }
 }
