e12dc88383994ea2bbac0a9fcca337203692bc10
noaione <noaione0809@gmail.com>
2023-11-29 10:17:38 +0700

    feat: implement image extractor for EPUB books
    This should fix some problems I'm having while also having extra legacy features.

--- komga/src/main/kotlin/org/gotson/komga/interfaces/api/opds/v1/OpdsController.kt
+++ komga/src/main/kotlin/org/gotson/komga/interfaces/api/opds/v1/OpdsController.kt
@@ -23,6 +23,7 @@ import org.gotson.komga.domain.persistence.MediaRepository
 import org.gotson.komga.domain.persistence.ReadListRepository
 import org.gotson.komga.domain.persistence.ReferentialRepository
 import org.gotson.komga.domain.persistence.SeriesCollectionRepository
+import org.gotson.komga.domain.service.BookAnalyzer
 import org.gotson.komga.domain.service.BookLifecycle
 import org.gotson.komga.infrastructure.configuration.KomgaSettingsProvider
 import org.gotson.komga.infrastructure.image.ImageType
@@ -120,6 +121,7 @@ class OpdsController(
   private val commonBookController: CommonBookController,
   private val komgaSettingsProvider: KomgaSettingsProvider,
   private val contentRestrictionChecker: ContentRestrictionChecker,
+  private val bookAnalyzer: BookAnalyzer,
   @Qualifier("pdfImageType")
   private val pdfImageType: ImageType,
 ) {
@@ -919,7 +921,8 @@ class OpdsController(
     val thumbnailMediaType =
       when (media.profile) {
         MediaProfile.PDF -> pdfImageType.mediaType
-        else -> "image/jpeg"
+        MediaProfile.EPUB -> bookAnalyzer.getPageInfoFromEpub(media, 1).mediaType ?: "image/jpeg"
+        else -> media.pages[0].mediaType
       }
 
     return OpdsEntryAcquisition(
